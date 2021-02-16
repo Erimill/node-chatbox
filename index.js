c25d2fa98f14fdb0926eeb9927db42a4cae95b8a
unknown <jmiller62@live.com>
2021-02-16 12:30:33 -0500

    base changes

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 const http = require('http').Server(app);
-const socket = require('socket.io')(http);
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const io = require('socket.io')(http);
 
-const server = app.listen(PORT, function () {
-  console.log(`Listening on port ${PORT}`);
-  console.log(`http://localhost:${PORT}`);
+app.get('/', function(req, res) {
+    res.render('index.ejs');
 });
 
-// Static files
-app.use(express.static("public"));
+io.sockets.on('connection', function(socket) {
+    socket.on('username', function(username) {
+        socket.username = username;
+        io.emit('is_online', '🔵 <i>' + socket.username + ' join the chat..</i>');
+    });
 
-// Socket setup
-const io = socket(server);
+    socket.on('disconnect', function(username) {
+        io.emit('is_online', '🔴 <i>' + socket.username + ' left the chat..</i>');
+    })
 
-io.on("connection", function (socket) {
-  console.log("Made socket connection");
+    socket.on('chat_message', function(message) {
+        io.emit('chat_message', '<strong>' + socket.username + '</strong>: ' + message);
+    });
+
+});
+
+const server = http.listen(8080, function() {
+    console.log('listening on *:8080');
 });
